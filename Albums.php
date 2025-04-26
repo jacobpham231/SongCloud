e3958d2580f80a758f8f94ece9b18c1f6fa0064a
JoshuaP066 <136041397+JoshuaP066@users.noreply.github.com>
2025-04-26 10:54:12 -0500

    Fixed an error

--- Albums.php
+++ Albums.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "SongCloud";
-$conn = new mysqli ($servername, $username, $password, $dbname) or die("Connect failed: %s\n." $conn -> error);
+$conn = new mysqli ($servername, $username, $password, $dbname) or die("Connect failed: %s\n". $conn -> error);
 
 $sql = "SELECT * FROM album";
 
