511cacca0eada48dcc5f49bb665d3c8395c18ec7
JoshuaP066 <136041397+JoshuaP066@users.noreply.github.com>
2025-04-26 10:48:34 -0500

    Added basic select all for testing

--- Albums.php
+++ Albums.php
@@ -1,7 +1,18 @@
-</php
+<?op0php
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "SongCloud";
 $conn = new mysqli ($servername, $username, $password, $dbname) or die("Connect failed: %s\n." $conn -> error);
+
+$sql = "SELECT * FROM album";
+
+$result = $conn -> query($sql);
+
+while ($row = $result -> fetch_assoc()) {
+  echo "album_id : ".$row['album_id']."<br>";
+  echo "name : ".$row['name']."<br>";
+  echo "song count : ".$row['num_songs']."<br>";
+  echo "release date : ".$row['release_date']."<br>";
+}
 ?>
