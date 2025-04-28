52ae4ba15e5366d8638ca909b1731fc885f5c612
JoshuaP066 <136041397+JoshuaP066@users.noreply.github.com>
2025-04-28 16:03:35 -0500

    Update Albums.php

--- Albums.php
+++ Albums.php
@@ -5,7 +5,7 @@ $password = "";
 $dbname = "SongCloud";
 $conn = new mysqli ($servername, $username, $password, $dbname) or die("Connect failed: %s\n". $conn -> error);
 
-$sql = "SELECT * FROM Album";
+$sql = "SELECT * FROM album";
 
 $result = $conn -> query($sql);
 
